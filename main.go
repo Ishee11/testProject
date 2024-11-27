b16cafe5afc83e4c1b9e2733eca2f5efcabd1a7c
Stepan Semenov <ishee11@gmail.com>
2024-11-27 15:03:25 +1000

    Версия с комментами

--- main.go
+++ main.go
@@ -2,20 +2,28 @@ package main
 
 import "fmt"
 
-func deleteRepit(nums []int) {
-	k := 1
-	for i := 1; i < len(nums)-1; i++ {
-		if nums[i] != nums[k-1] {
-			nums[k] = nums[i]
-			k++
+func deleteDuplicates(nums []int) {
+	if len(nums) == 0 {
+		return
+	}
+	// Указатель на уникальный элемент.
+	uniqueIndex := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[uniqueIndex-1] {
+			nums[uniqueIndex] = nums[i]
+			uniqueIndex++
 		}
 	}
-	fmt.Println(k)
+	fmt.Printf("Количество уникальных чисел: %v\n", uniqueIndex)
 }
 
 func main() {
 	fmt.Println("Version 4")
 	array := []int{0, 1, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5}
-	deleteRepit(array)
-	fmt.Println(array)
+
+	// Обработка массива.
+	deleteDuplicates(array)
+
+	// Вывод результата.
+	fmt.Println("Array without duplicates:", array)
 }
