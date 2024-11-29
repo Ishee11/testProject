1023fe383a1038ad28509a8cc7ca60cdbd907c51
Stepan Semenov <ishee11@gmail.com>
2024-11-29 10:29:04 +1000

    merge

--- main.go
+++ main.go
@@ -1,6 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// QuickSort сортирует слайс "на месте".
+func QuickSort(arr []int) {
+	if len(arr) < 2 {
+		return // Уже отсортировано
+	}
+
+	// Выбор pivot (опорного элемента)
+	pivotIndex := len(arr) / 2
+	pivot := arr[pivotIndex]
+
+	// Разделение массива
+	left, right := 0, len(arr)-1
+
+	// Перемещаем элементы относительно pivot
+	for left <= right {
+		// Сдвигаем указатели до тех пор, пока не найдём элемент для обмена
+		for arr[left] < pivot {
+			left++
+		}
+		for arr[right] > pivot {
+			right--
+		}
+		if left <= right {
+			arr[left], arr[right] = arr[right], arr[left]
+			left++
+			right--
+		}
+	}
+
+	// Рекурсивно сортируем две части
+	QuickSort(arr[:right+1]) // Левая часть
+	QuickSort(arr[left:])    // Правая часть
+}
 
 func deleteDuplicates(nums []int) {
 	if len(nums) == 0 {
@@ -18,6 +54,12 @@ func deleteDuplicates(nums []int) {
 }
 
 func main() {
+	data := []int{6, 9, 4, 2, 5, 3, 7, 0, 1, 8}
+	fmt.Println("До сортировки:", data)
+	QuickSort(data)
+	fmt.Println("После сортировки:", data)
+
+	// -----------------------------------------------
 	fmt.Println("Version 4")
 	array := []int{0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5}
 
